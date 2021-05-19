7e9258f4747458e6de75e8eedb0c2cc052377f77
HEMMER-FX <hemmerfrancoisxavier@gmail.com>
2021-05-19 17:50:58 +0200

    ajout program controller + recherche dans barre url

---
+++ src/Controller/ProgramController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProgramController extends AbstractController
+{
+    /**
+     * @Route("/programs/", name="program_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('program/index.html.twig', [
+            "website" => "Wild Séries",
+        ]);
+    }
+    /**
+     * @Route("/programs/{page}", requirements={"page"="\d+"}, methods={"GET"},name="program_show")
+    */
+    public function show(int $page): Response
+    {
+        return $this->render('program/show.html.twig', ['page' => $page]);
+    }
+}
