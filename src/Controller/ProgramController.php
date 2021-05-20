ca54742f462c4e2f59b4814d677a46bffd08e431
HEMMER-FX <hemmerfrancoisxavier@gmail.com>
2021-05-20 09:38:51 +0200

    formulaire fonctionnel

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -3,25 +3,29 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProgramController extends AbstractController
 {
-    /**
-     * @Route("/programs/", name="program_index")
-     */
-    public function index(): Response
-    {
-        return $this->render('program/index.html.twig', [
-            "website" => "Wild Séries",
-        ]);
-    }
+
     /**
      * @Route("/programs/{page}", requirements={"page"="\d+"}, methods={"GET"},name="program_show")
     */
     public function show(int $page): Response
     {
         return $this->render('program/show.html.twig', ['page' => $page]);
+    }
+
+    /**
+     * @Route("/programs/", name="test_program")
+     */
+    public function test(): Response
+    {
+        $request = Request::createFromGlobals();
+
+        $programe = $request->query->get('programs',null);
+        return $this->render('program/index.html.twig', ['Programs_demande' => $programe]);
     }
 }
